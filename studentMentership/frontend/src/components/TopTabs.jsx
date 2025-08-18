4b67cacbb5b595a5ad4e794d0fc10bf8eaf5b005
kaleb <kalebasmamaw21@gmail.com>
2025-08-18 15:40:40 +0300

    Add header on mentee page

--- studentMentership/frontend/src/components/TopTabs.jsx
+++ studentMentership/frontend/src/components/TopTabs.jsx
@@ -1,19 +1,28 @@
 // components/TopTabs.jsx
 import React from "react";
+import {Bell, Search, User} from "lucide-react";
 
-const tabs = ["Your Progress", "Academic Goals", "Session Attended", "Skills Development"];
+
+
 
 export default function TopTabs() {
-  return (
-    <div className="border bg-card rounded-md flex">
-      {tabs.map((tab, i) => (
-        <button
-          key={i}
-          className="flex-1 py-3 text-center hover:bg-primary-foreground transition"
-        >
-          {tab}
-        </button>
-      ))}
+   return (
+    <div className="flex items-center justify-between  pl-65 pr-15 py-4 bg-white shadow-md">
+      
+      <div className="flex items-center gap-2">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <Search className="w-5 h-5 text-gray-500" />
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Bell className="w-6 h-6 text-gray-600 cursor-pointer hover:text-blue-500 transition-colors" />
+        <User className="w-6 h-6 text-gray-600 cursor-pointer hover:text-blue-500 transition-colors" />
+      </div>
+      
     </div>
   );
 }
