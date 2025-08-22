f56fcc6f6ebd41ee28e46be650d93cb213f570ba
Tewuhbo Mihret <tewuhbo.mihret@chromaway.com>
2025-08-22 23:06:05 +0300

    student page added

---
+++ studentMentership/frontend/src/pages/Student.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StudentDashboard } from '../components/student/Student-dashboard'
+
+function Student() {
+  return (
+    <div className='min-h-screen bg-background'>
+      <StudentDashboard />
+    </div>
+  )
+}
+
+export default Student
