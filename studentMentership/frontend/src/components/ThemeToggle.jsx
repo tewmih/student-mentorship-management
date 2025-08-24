2f80a5eff8d591e5635748d7fddc78dce816e56e
Tewuhbo Mihret <tewuhbo.mihret@chromaway.com>
2025-08-24 14:59:23 +0300

    bar graph and reusable card of stat

--- studentMentership/frontend/src/components/ThemeToggle.jsx
+++ studentMentership/frontend/src/components/ThemeToggle.jsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { Sun, Moon } from 'lucide-react';
-import { cn } from '../lib/utils';
+import { cn } from '../utils/utils';
 
 
 const ThemeToggle = () =>{
