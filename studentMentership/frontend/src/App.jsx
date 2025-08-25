cb9a6036dfc93a758e7e1c7a7b5325fd30a1cd7c
Tewuhbo Mihret <tewuhbo.mihret@chromaway.com>
2025-08-25 13:15:54 +0300

    header and footer added

--- studentMentership/frontend/src/App.jsx
+++ studentMentership/frontend/src/App.jsx
@@ -4,10 +4,13 @@ import LoginPopup from "./components/LoginPopUp.jsx";
 import Mentee from "./pages/Mentee.jsx";
 import MentorPage from "./pages/MentorPage.jsx";
 import StudentUnion from "./pages/StudentUnion/StudentUnion.jsx";
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.jsx";
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menteedashboard" element={<Mentee />} />
@@ -15,6 +18,7 @@ function App() {
           <Route path="/mentordashboard" element={<MentorPage />}  />
           <Route path="/studentunion" element={<StudentUnion />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
