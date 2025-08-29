9ec69509029a25c71b467e0a93480a02aeb77f7d
Tewuhbo Mihret <tewuhbo.mihret@chromaway.com>
2025-08-29 09:09:11 +0300

    profile page

---
+++ studentMentership/frontend/src/pages/profile/profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div className='w-full h-full bg-red-500 flex items-center justify-center'>
+      <h1 className='text-white text-2xl font-bold'>Profile Page</h1>
+    </div>
+  )
+}
+
+export default Profile
